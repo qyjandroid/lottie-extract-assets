39018d92f474945054c9e7f884cd16a704ba9f53
quanyj <qyjandroid@163.com>
2021-05-10 09:49:43 +0800

    设置默认配置文件，支持多项目

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # lottie-extract-assets
 根据lottie配置文件，提取lottie的json文件中图片，输出配置
 
+在项目根目录下创建 lottie-extract-assets.json 编写配置
+```json
+    [
+        //支持配置多个项目
+       {
+            "configPath":"src/act1/lottieConfig.json", //需要提取的lottie 配置
+            "outFileName":"lottie-assets.js", //生成的文件名（.js或者.ts）
+            "to":"dist", //输出资源文件保存目录
+            "globalName":"widow._LottieConfig",
+            "absPath":1,//lottie图片资源是否生成绝对路径  1（是） 2（否）
+       }
+    ]
+```
+lottieConfig.json编写格式如下：
+```json
+[
+    "https://xxx.com/ceremonyBlessingBagFirst/data.json"
+]
+```
+
 运行命令：
+```js
 lottieExtract 
-根据提示输入
-请输入LottieConfig文件目录：(src/lottieConfig.json) ./lottieConfig.json
-请输入输出资源文件名称：(lottie-assets.js) lottie-assets.js
-请输入输出资源文件保存目录：(dist/lottie) dist/lottie
-请输入全局访问提取资源对象：(window._config) widow._LottieConfig
+```
+
+检查如果没有查找到lottie-extract-assets.json,则需要根据提示输入:
+> * 请输入LottieConfig文件目录：(src/lottieConfig.json) ./lottieConfig.json
+> * 请输入输出资源文件名称：(lottie-assets.js) lottie-assets.js
+> * 请输入输出资源文件保存目录：(dist) dist/lottie
+> * 请输入全局访问提取资源对象：(window._config) widow._LottieConfig
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -7,66 +7,102 @@ const program = require('commander');
 const inquirer = require('inquirer');
 const cwd = process.cwd();
 
-program.version('v' + require('./package.json').version, '-v, --version')
-	.action(() => {
-			inquirer.prompt([
-				{
-                    type:'input',
-					name: 'configPath',
-					message: '请输入LottieConfig文件目录：(src/lottieConfig.json)',
-                    validate:val=>{
-                        if (fs.existsSync(val)) {
-                            return true;
-                          }
-                        return '文件不存在，请输入正确文件目录'
-                    }
-				},
-                {
-                    type:'input',
-					name: 'outFileName',
-					message: '请输入输出资源文件名称：(lottie-assets.js)',
-				},
-				{
-                    type:'input',
-					name: 'to',
-					message: '请输入输出资源文件保存目录：(dist/lottie)'
-				},
-                {
-                    type:'input',
-					name: 'globalName',
-					message: '请输入全局访问提取资源对象：(window._config)'
-				},
-                {
-                    type:'rawlist',
-                    message:'图片资源是否生成绝对路径',
-                    name:'absPath',
-                    choices:[
+//默认读取的配置文件
+const defaultConfigJsonName="lottie-extract-assets.json";
+const defaultConfigJson =path.join(cwd,defaultConfigJsonName);
+console.log("读取配置文件：",defaultConfigJson);
+//支持同时编译多个项目lottie资源提取
+readJsonFile(defaultConfigJson).then((result)=>{
+    if(result){
+        startAssetsExtract(result);
+    }else{
+        console.log(defaultConfigJsonName+"文件不存在,请按照以下提示操作")
+        program.version('v' + require('./package.json').version, '-v, --version')
+            .action(() => {
+                    inquirer.prompt([
                         {
-                            key:"yes",
-                            name:"是",
-                            value:1,
-                            checked: true // 默认选中
+                            type:'input',
+                            name: 'configPath',
+                            message: '请输入LottieConfig文件path：(例如：src/lottieConfig.json)',
+                            validate:val=>{
+                                if (fs.existsSync(val)) {
+                                    return true;
+                                }
+                                return '文件不存在，请输入正确文件path'
+                            }
                         },
                         {
-                            key:"no",
-                            name:"否",
-                            value:2,
+                            type:'input',
+                            name: 'outFileName',
+                            message: '请输入输出资源文件名称：(例如：lottie-assets.js)',
+                        },
+                        {
+                            type:'input',
+                            name: 'to',
+                            message: '请输入输出资源文件保存目录：(例如：dist)'
+                        },
+                        {
+                            type:'input',
+                            name: 'globalName',
+                            message: '请输入全局访问提取资源对象：(例如：window._config)'
+                        },
+                        {
+                            type:'rawlist',
+                            message:'图片资源是否生成绝对路径',
+                            name:'absPath',
+                            choices:[
+                                {
+                                    key:"yes",
+                                    name:"是",
+                                    value:1,
+                                    checked: true // 默认选中
+                                },
+                                {
+                                    key:"no",
+                                    name:"否",
+                                    value:2,
+                                }
+                            ]
                         }
-                    ]
-                }
+                        
+                        
+                    ]).then(async(answers)  =>  {
+                        extractAssets(answers,()=>{});
+                    }).catch((e)=>{
+                        console.log("错误信息：",e);
+                    })
                 
-				
-			]).then(async(answers)  =>  {
-                const config=initPath(answers);
-               const  imgArray= await readJsonFile(config);
-               saveFile(imgArray,config.globalName,config.outFileName);
+            });
+        program.parse(process.argv);
+    }
+});
 
-			}).catch((e)=>{
-                console.log("错误信息：",e);
-            })
-		
-	});
-program.parse(process.argv);
+
+function startAssetsExtract(configJson){
+    let count=0;
+    configJson.map((item,key)=>{
+        extractAssets(item,()=>{
+            count++;
+            if(count===configJson.length){
+                console.log("all over out")
+            }
+        });
+    })
+}
+
+
+function extractAssets(answers,callback) {
+    const config=initPath(answers);
+    readJsonFile(config.configPath).then(async (lottieConfig) =>{
+        if(lottieConfig){
+            //根据配置获取资源链接(包含当前的lottie和lottie中图片)
+            const imgArray = await getLink(lottieConfig,config.isAbsPath);
+            saveFile(imgArray,config.globalName,config.outFileName,callback);
+        }
+    });
+}
+
+
 
 
 function mkDirsSync(dirname) {
@@ -213,10 +249,9 @@ function mkDirsSync(dirname) {
      * 读取配置文件,生成js文件。
      * @memberOf LottieExtractAssetsPlugin
      */
-      async function readJsonFile (config) {
-          const {configPath,isAbsPath}=config;
+    function readJsonFile (configPath) {
         //获取配置
-        let lottieConfig = await new Promise((resolve, reject) => {
+        return  new Promise((resolve, reject) => {
             try {
                 //读取配置文件
                 fs.readFile(configPath, (err, data) => {
@@ -231,29 +266,27 @@ function mkDirsSync(dirname) {
             } catch (e) {
                 reject(e);
             }
-        }).catch(()=>{
+        }).catch((e)=>{
             console.warn("读取配置文件错误:"+ configPath);
         });
-        if(!lottieConfig){
-            return;
-        }
-        //根据配置获取资源链接(包含当前的lottie和lottie中图片)
-        const imgLink = await getLink(lottieConfig,isAbsPath);
-        return imgLink;
        
     }
 
-    saveFile=(imgLink,globalName,outFileName)=>{
+
+
+
+    function saveFile(imgLink,globalName,outFileName,callback){
          // 采用js文件，方便我们前端代码集成使用。
          let content = globalName + " = " + JSON.stringify(imgLink, null, 4) + ";";
-         console.log("内容",content);
- 
          fs.writeFile(outFileName,content,'utf-8',function(err){
              if(err){
                  throw err;
                  return;
              }
              console.log("out over: ",outFileName);
+             if(callback){
+                callback(outFileName);
+             }
          })
     }
   
